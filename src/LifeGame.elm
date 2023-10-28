a67a6470a344b9f60dfcee3e81e9cb802ac851c6
nishink <nishink@hotmail.com>
2023-10-28 19:47:37 +0900

    add function comment

--- src/LifeGame.elm
+++ src/LifeGame.elm
@@ -17,45 +17,65 @@ main =
 -- MODEL
 
 
+{-| セルの位置を表す座標
+(x, y)
+-}
 type alias Pos =
     ( Number, Number )
 
 
+{-| セルを配置する盤面
+座標のリストで表す
+-}
 type alias Board =
     List Pos
 
 
+{-| アプリケーションの状態
+board : 盤面の状態
+pause : 一時停止状態
+-}
 type alias Model =
     { board : Board
     , pause : Bool
     }
 
 
-
--- 初期状態
-
-
+{-| 初期状態
+盤面にはグライダーを配置
+一時停止状態はFalse(停止していない)
+-}
 init : Model
 init =
     Model [ ( 3, 3 ), ( 3, 4 ), ( 3, 5 ), ( 4, 5 ), ( 5, 4 ) ] False
 
 
+{-| 画面の幅
+-}
 windowWidth =
     width * size
 
 
+{-| 画面の高さ
+-}
 windowHeight =
     height * size
 
 
+{-| １つのセルのサイズ
+-}
 size =
     16
 
 
+{-| 横にセルが並ぶ数
+-}
 width =
     40
 
 
+{-| 縦にセルが並ぶ数
+-}
 height =
     30
 
@@ -65,16 +85,18 @@ height =
 -- VIEW
 
 
+{-| 描画処理
+表示するものをリストで連結(::)
+-}
+view : a -> Model -> List Shape
 view _ model =
     drawPauseButton model
         :: rectangle (rgb 240 240 240) windowWidth windowHeight
         :: List.map drawPos model.board
 
 
-
--- セル一つ分の描画
-
-
+{-| セル一つ分を描画
+-}
 drawPos : Pos -> Shape
 drawPos ( x, y ) =
     let
@@ -88,10 +110,8 @@ drawPos ( x, y ) =
         |> move dx dy
 
 
-
--- 一時停止ボタン
-
-
+{-| 一時停止ボタン
+-}
 drawPauseButton : Model -> Shape
 drawPauseButton model =
     let
@@ -123,50 +143,33 @@ drawPauseButton model =
 -- UPDATE
 
 
+{-| 更新処理
+-}
 update computer model =
     let
+        click =
+            computer.mouse.click
+
+        x =
+            computer.mouse.x
+
+        y =
+            computer.mouse.y
+
         -- ボタンクリックで一時停止
         pause =
-            if computer.mouse.click then
-                let
-                    px =
-                        computer.mouse.x
-
-                    py =
-                        computer.mouse.y
-
-                    by =
-                        windowHeight * 0.5 + 64
-                in
-                if -128 <= px && px < 128 && by - 32 <= py && py < by + 32 then
-                    not model.pause
-
-                else
-                    model.pause
-
-            else
-                model.pause
+            updatePause click x y model.pause
 
         -- 盤面をクリックで種を蒔く
         board =
-            if computer.mouse.click then
-                let
-                    ( px, py ) =
-                        pointToPos computer.mouse.x computer.mouse.y
-                in
-                if 0 <= px && px < width && 0 <= py && py < height then
-                    click ( px, py ) model.board
-
-                else
-                    model.board
-
-            else
-                model.board
+            updateBoard click x y model.board
     in
     if model.pause then
+        -- 停止状態の時は状態を更新しない
         { model | board = board, pause = pause }
 
     else
+        -- 活動状態の時は状態を（生存＋誕生）に更新する
         { model
             | board =
                 survivors board ++ births board
@@ -174,22 +177,56 @@ update computer model =
         }
 
 
+{-| ボタンクリックで一時停止
+-}
+updatePause : Bool -> Number -> Number -> Bool -> Bool
+updatePause click x y pause =
+    if click then
+        let
+            by =
+                windowHeight * 0.5 + 64
+        in
+        if -128 <= x && x < 128 && by - 32 <= y && y < by + 32 then
+            not pause
 
--- クリックした座標をPosに変換
+        else
+            pause
+
+    else
+        pause
 
 
+{-| 盤面をクリックで種を蒔く
+-}
+updateBoard : Bool -> Number -> Number -> Board -> Board
+updateBoard click x y board =
+    if click then
+        let
+            ( px, py ) =
+                pointToPos x y
+        in
+        if 0 <= px && px < width && 0 <= py && py < height then
+            clickCell ( px, py ) board
+
+        else
+            board
+
+    else
+        board
+
+
+{-| クリックした座標をPosに変換
+-}
 pointToPos : Number -> Number -> Pos
 pointToPos x y =
     ( div (x + windowWidth * 0.5) size, div (y + windowHeight * 0.5) size )
 
 
-
--- セルをクリックした時の処理
--- そのセルが不在なら誕生、生存なら消滅させる
-
-
-click : Pos -> Board -> Board
-click pos board =
+{-| セルをクリックした時の処理
+そのセルが不在なら誕生、生存なら消滅させる
+-}
+clickCell : Pos -> Board -> Board
+clickCell pos board =
     if isEmpty board pos then
         pos :: board
 
@@ -198,27 +235,26 @@ click pos board =
 
 
 
--- 生存判定：pがbの要素であれば生存と判定
+--
 
 
+{-| 生存判定
+pがbの要素であれば生存と判定
+-}
 isAlive : Board -> Pos -> Bool
 isAlive board pos =
     List.filter (\p -> p == pos) board /= []
 
 
-
--- 死滅判定：生存の逆
-
-
+{-| 死滅判定：生存の逆
+-}
 isEmpty : Board -> Pos -> Bool
 isEmpty board pos =
     not (isAlive board pos)
 
 
-
--- 画面外をループさせる
-
-
+{-| 画面外をループさせる
+-}
 wrap : Pos -> Pos
 wrap ( x, y ) =
     ( mod x width, mod y height )
@@ -228,6 +264,8 @@ wrap ( x, y ) =
 -- Number型に使える整数の割り算がないので自作
 
 
+{-| Number型に対する割り算の剰余
+-}
 mod : Number -> Number -> Number
 mod x y =
     if x < 0 then
@@ -240,6 +278,8 @@ mod x y =
         mod (x - y) y
 
 
+{-| Number型に対する割り算の商
+-}
 div : Number -> Number -> Number
 div x y =
     if x < 0 then
@@ -252,10 +292,8 @@ div x y =
         1 + div (x - y) y
 
 
-
--- 周囲８方向を取得
-
-
+{-| 周囲８方向を取得
+-}
 neighbors : Pos -> List Pos
 neighbors ( x, y ) =
     List.map wrap
@@ -270,10 +308,8 @@ neighbors ( x, y ) =
         ]
 
 
-
--- 周囲に生きているセルの個数を取得
-
-
+{-| 周囲の生きているセルの個数を取得
+-}
 liveneighbors : Board -> Pos -> Int
 liveneighbors board pos =
     neighbors pos
@@ -281,19 +317,15 @@ liveneighbors board pos =
         |> List.length
 
 
-
--- 現在の状態から生存となるものを抽出
-
-
+{-| 現在の状態から生存となるものを抽出
+-}
 survivors : Board -> List Pos
 survivors board =
     List.filter (survive board) board
 
 
-
--- 生存判定（周囲の生存者が２か３）
-
-
+{-| 生存判定（周囲の生存数が２か３）
+-}
 survive : Board -> Pos -> Bool
 survive board pos =
     let
@@ -303,10 +335,8 @@ survive board pos =
     count == 2 || count == 3
 
 
-
--- 重複を削除(remove duplicates)
-
-
+{-| 重複を削除(remove duplicates)
+-}
 rmdups : List a -> List a
 rmdups list =
     case list of
@@ -317,10 +347,8 @@ rmdups list =
             x :: rmdups (List.filter (\y -> y /= x) xs)
 
 
-
--- 現在の状態から誕生するものを抽出
-
-
+{-| 現在の状態から誕生するものを抽出
+-}
 births : Board -> List Pos
 births board =
     List.concatMap neighbors board
